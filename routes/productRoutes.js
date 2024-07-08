c86e45cf21eb1371d1dbf02557b441585e0a2f1f
HUN-sp <chopravinaykumarchopra@gmail.com>
2024-07-08 11:18:54 +0530

    First Commit

---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+
+const router = require('express').Router()
+
+
+
+router.post("/api/products", );
+ 
+ 
+ // get route
+ 
+ router.get('/api/products' , )
+ 
+ // Get product by id
+ 
+ router.get('/api/products/:id' , )
+ 
+ // Update product
+ 
+ router.put('/api/products/:id' , )
+ 
+ 
+ /// Delete a Resource
+ 
+ router.delete('/api/products/:id' , )
+ 
+ 
